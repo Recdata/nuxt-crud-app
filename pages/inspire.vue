d69924c943a69e2aef3f9a79a71f02aa9380399e
amanadeem <quranrecitation80@gmail.com>
2024-07-24 14:00:08 +0500

    next crud app

---
+++ pages/inspire.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem in your mind. Then, write the code and do practical implementation.&#8221;
+        <footer>
+          <small>
+            <em>Hakimi Altumush</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'InspirePage'
+}
+</script>
